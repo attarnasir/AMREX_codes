09c9ff82e4390f85034cee79bcc41ae4670a96f2
Nasir Attar <attarnasir786@gmail.com>
2022-12-22 17:40:15 +0530

    AMR

---
+++ Amr/Advection_AmrCore/Exec/tmp_build_dir/o/2d.gnu.MPI.EXE/AMReX_BArena.d
@@ -0,0 +1,30 @@
+tmp_build_dir/o/2d.gnu.MPI.EXE/AMReX_BArena.o: \
+ ../../../../../amrex/Src/Base/AMReX_BArena.cpp \
+ ../../../../../amrex/Src/Base/AMReX_BArena.H \
+ tmp_build_dir/s/2d.gnu.MPI.EXE/AMReX_Config.H \
+ ../../../../../amrex/Src/Base/AMReX_Arena.H \
+ ../../../../../amrex/Src/Base/AMReX_BLassert.H \
+ ../../../../../amrex/Src/Base/AMReX.H \
+ ../../../../../amrex/Src/Base/AMReX_GpuQualifiers.H \
+ ../../../../../amrex/Src/Base/AMReX_GpuPrint.H \
+ ../../../../../amrex/Src/Base/AMReX_GpuAssert.H \
+ ../../../../../amrex/Src/Base/AMReX_ccse-mpi.H \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h \
+ ../../../../../amrex/Src/Base/AMReX_Exception.H \
+ ../../../../../amrex/Src/Base/AMReX_Extension.H \
+ ../../../../../amrex/Src/Base/AMReX_INT.H
+../../../../../amrex/Src/Base/AMReX_BArena.H:
+tmp_build_dir/s/2d.gnu.MPI.EXE/AMReX_Config.H:
+../../../../../amrex/Src/Base/AMReX_Arena.H:
+../../../../../amrex/Src/Base/AMReX_BLassert.H:
+../../../../../amrex/Src/Base/AMReX.H:
+../../../../../amrex/Src/Base/AMReX_GpuQualifiers.H:
+../../../../../amrex/Src/Base/AMReX_GpuPrint.H:
+../../../../../amrex/Src/Base/AMReX_GpuAssert.H:
+../../../../../amrex/Src/Base/AMReX_ccse-mpi.H:
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
+../../../../../amrex/Src/Base/AMReX_Exception.H:
+../../../../../amrex/Src/Base/AMReX_Extension.H:
+../../../../../amrex/Src/Base/AMReX_INT.H:
